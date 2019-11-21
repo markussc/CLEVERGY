44a16a7c7787bc8c610cfaa33376c375c8c091c0
Markus Schafroth <m.schafroth.ch@gmail.com>
2019-11-21 20:57:38 +0100

    [SmartFox] first draft for SmartFox PRO support

--- src/AppBundle/Utils/Connectors/SmartFoxConnector.php
+++ src/AppBundle/Utils/Connectors/SmartFoxConnector.php
@@ -16,15 +16,19 @@ class SmartFoxConnector
     protected $browser;
     protected $basePath;
     protected $ip;
+    protected $version;
 
     public function __construct(EntityManager $em, \Buzz\Browser $browser, Array $connectors)
     {
         $this->em = $em;
         $this->browser = $browser;
+        $this->ip = null;
+        $this->version = null;
         if (array_key_exists('smartfox', $connectors)) {
             $this->ip = $connectors['smartfox']['ip'];
-        } else {
-            $this->ip = null;
+            if (array_key_exists('version', $connectors['smartfox'])) {
+                $this->version = $connectors['smartfox']['version'];
+            }
         }
         $this->basePath = 'http://' . $this->ip;
     }
@@ -41,8 +45,11 @@ class SmartFoxConnector
 
     public function getAll($calculatedData = false)
     {
-        $responseJson = $this->browser->get($this->basePath . '/all')->getContent();
-        $responseArr = json_decode($responseJson, true);
+        if ($this->version === "pro") {
+            $responseArr = $this->getFromPRO();
+        } else {
+            $responseArr = $this->getFromREG9TE();
+        }
 
         // if requested, add calculated data
         if ($calculatedData) {
@@ -55,5 +62,43 @@ class SmartFoxConnector
     public function getIp()
     {
         return $this->ip;
+    }
+
+    private function getFromREG9TE()
+    {
+        return json_decode($this->browser->get($this->basePath . '/all')->getContent(), true);
+    }
+
+    private function getFromPRO()
+    {
+        $xmlData = $this->browser->get($this->basePath . '/values.xml')->getContent();
+        $xml = new \SimpleXMLElement($xmlData);
+        $data = [];
+        foreach ($xml->children() as $value) {
+            $val = (string)$value;
+            if (strpos($val, " kW") !== false) {
+                $val = 1000*substr($val, 0, strpos($val, " kW"));
+            } elseif (strpos($val, " kWh") !== false) {
+                $val = 1000*substr($val, 0, strpos($val, " kWh"));
+            } elseif (strpos($val, " Wh") !== false) {
+                $val = 1*substr($val, 0, strpos($val, " Wh"));
+            } elseif (strpos($val, " °C") !== false) {
+                $val = 1*substr($val, 0, strpos($val, " °C"));
+            } elseif (strpos($val, " W") !== false) {
+                $val = 1*substr($val, 0, strpos($val, " W"));
+            }
+            $data[(string)$value['id']] = $val;
+        }
+
+        return [
+            "energy_in" => $data["u5827-41"],
+            "energy_out" => $data["u5824-41"],
+            "day_energy_in" => $data["u5863-41"],
+            "day_energy_out" => $data["u5872-41"],
+            "power_io" => $data["u5790-41"],
+            "digital" => ["0" => ["state" => $data["u5674-41"]]],
+            "PvPower" => ["0" => $data["u5272-41"]],
+            "PvEnergy" => ["0" => $data["u7015"]],
+        ];
     }
 }
