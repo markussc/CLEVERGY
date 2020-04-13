e60ef4b15e3331d3e1fec01d2e915d7b7c41ea07
Markus Schafroth <m.schafroth.ch@gmail.com>
2020-04-13 18:10:22 +0200

    [DataStoreBase] introduced new combined index

--- src/AppBundle/Entity/DataStoreBase.php
+++ src/AppBundle/Entity/DataStoreBase.php
@@ -10,7 +10,7 @@ use Symfony\Component\Validator\Constraints as Assert;
  * Base class for custom field tables
  *
  * @ORM\Entity
- * @ORM\Table(name="data_store", indexes={@ORM\Index(name="timestamp_idx", columns={"timestamp"}),@ORM\Index(name="connector_idx", columns={"connector_id"})})
+ * @ORM\Table(name="data_store", indexes={@ORM\Index(name="timestamp_idx", columns={"timestamp"}),@ORM\Index(name="connector_idx", columns={"connector_id"}),@ORM\Index(name="connector_timestamp_idx", columns={"connector_id", "timestamp"})})
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn("discr_type", type="string")
  * @ORM\HasLifecycleCallbacks
