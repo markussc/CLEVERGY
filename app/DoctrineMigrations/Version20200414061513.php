900d3757606210cc6e66c9eb77af39ba41862463
Markus Schafroth <m.schafroth.ch@gmail.com>
2020-04-14 09:09:15 +0200

    Migrations: added additional indexes

---
+++ app/DoctrineMigrations/Version20200414061513.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200414061513 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE INDEX discr_type_connector_idx ON data_store (discr_type, connector_id)');
+        $this->addSql('CREATE INDEX discr_type_connector_idx_timestamp ON data_store (discr_type, connector_id, timestamp)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX discr_type_connector_idx ON data_store');
+        $this->addSql('DROP INDEX discr_type_connector_idx_timestamp ON data_store');
+    }
+}
