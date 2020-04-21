540fdedd6ecbec8927f810d902e3fe57a513004d
Markus Schafroth <m.schafroth.ch@gmail.com>
2020-04-21 10:05:30 +0200

    [PcoWeb] restore accidentially deleted repository method

--- src/AppBundle/Entity/PcoWebDataStoreRepository.php
+++ src/AppBundle/Entity/PcoWebDataStoreRepository.php
@@ -22,4 +22,16 @@ class PcoWebDataStoreRepository extends DataStoreBaseRepository
             return $latest[0]->getData();
         }
     }
+
+    public function getLatestNotStatus($ip, $notStatus)
+    {
+        $qb = $this->createQueryBuilder('e')
+            ->where('e.connectorId = :ip')
+            ->andWhere('e.jsonValue NOT LIKE :status')
+            ->orderBy('e.timestamp', 'desc')
+            ->setParameter('ip', $ip)
+            ->setParameter('status', '%"' . $notStatus . '"%')
+            ->setMaxResults(1);
+        return $qb->getQuery()->getResult();
+    }
 }
