be24f15561d3a4f683fde77d38566410ffc17927
Markus Schafroth <m.schafroth.ch@gmail.com>
2020-12-23 23:21:09 +0100

    [WEM] added hint regarding chromium browser requirement on the web server

--- src/Utils/Connectors/WemConnector.php
+++ src/Utils/Connectors/WemConnector.php
@@ -8,6 +8,7 @@ use HeadlessChromium\BrowserFactory;
 
 /**
  * Connector to retrieve data from the WEM Portal (Weishaupt Energy Manager)
+ * Note: requires chromium browser installed on the system. Ubuntu: sudo snap install chromium
  *
  * @author Markus Schafroth
  */
