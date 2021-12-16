a99adaaaf968ca7c77523e588c79d11b67cfac33
Markus Schafroth <markus.schafroth@3084.ch>
2021-12-16 21:56:26 +0100

    [Symfony] fixes for Symfony 5, added password reset functionality (hidden)

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
