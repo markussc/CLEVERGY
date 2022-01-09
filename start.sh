8e77eb8c5e3303193f7269ec7bc2f85803f7bf8e
Markus Schafroth <markus.schafroth@3084.ch>
2022-01-09 15:00:19 +0100

    [Docker] fix for wrong syntax

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 cd "$(dirname "$(realpath "$0")")";
 
 # build docker containers
-docker-compose --file docker-compose.yml --env-file ./.env.local --no-cache build
+docker-compose --file docker-compose.yml --env-file ./.env.local build --no-cache
 
 # start docker-compose
 docker-compose --file docker-compose.yml --env-file ./.env.local up -d
