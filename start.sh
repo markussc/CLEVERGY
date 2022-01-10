7b7655eb3ea5cef173de40da34b5b787e039b012
Markus Schafroth <markus.schafroth@3084.ch>
2022-01-10 15:11:24 +0100

    [Docker] handle different instances correctly

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 cd "$(dirname "$(realpath "$0")")";
 
 # build docker containers
-docker-compose --file docker-compose.yml --env-file ./.env.local build --no-cache
+docker-compose --file docker-compose.yml --env-file ./.env.local build
 
 # start docker-compose
 docker-compose --file docker-compose.yml --env-file ./.env.local up -d
