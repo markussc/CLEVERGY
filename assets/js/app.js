b16f759a3d65614a7760a1964fa751d80b69a93d
Markus Schafroth <markus.schafroth@3084.ch>
2022-01-12 11:16:24 +0100

    [Dependencies] including foundation + fontawesome via yarn

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,16 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import '../css/app.css';
+import '../css/global.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 import $ from 'jquery';
 
 // create global $ and jQuery variables
 global.$ = global.jQuery = $;
+
+// foundation
+require('foundation')
 
 require('../css/base.css');
 require('../css/modal.css');
