334b850d54842c6c31b452f18bc4db358a3b445e
Markus Schafroth <markus.schafroth@3084.ch>
2022-01-19 13:04:53 +0100

    [DataStore] revert to timestamp sorting

--- src/Repository/DataStoreBaseRepository.php
+++ src/Repository/DataStoreBaseRepository.php
@@ -13,15 +13,15 @@ class DataStoreBaseRepository extends EntityRepository
     {
         $qb = $this->createQueryBuilder('e')
             ->where('e.connectorId = :ip')
-            ->orderBy('e.id', 'desc')
+            ->orderBy('e.timestamp', 'desc')
             ->setParameter('ip', $ip)
             ->setMaxResults(1);
 
-        $latest = $qb->getQuery()->getResult();
-        if (!count($latest)) {
+        $latest = $qb->getQuery()->getOneOrNullResult();
+        if ($latest == null) {
             return 0;
         } else {
-            return $latest[0]->getData();
+            return $latest->getData();
         }
     }
 
@@ -29,7 +29,7 @@ class DataStoreBaseRepository extends EntityRepository
     {
         $qb = $this->createQueryBuilder('e')
             ->where('e.connectorId = :ip')
-            ->orderBy('e.id', 'desc')
+            ->orderBy('e.timestamp', 'desc')
             ->setParameter('ip', $ip)
             ->setMaxResults(1);
 
