637ed244a58b689bd9f6f8d164923e46cfa44fe5
Markus Schafroth <markus.schafroth@3084.ch>
2024-05-02 11:21:29 +0200

    [Storage] bugfix

--- src/Utils/Connectors/SmartFoxConnector.php
+++ src/Utils/Connectors/SmartFoxConnector.php
@@ -236,7 +236,7 @@ class SmartFoxConnector
                     $msg = 'Excess cell temperature, do not use battery until normalized';
                 }
                 $config = $this->getConfig();
-                if ($msg === null && ((($power > 50 && $idleType == 'charge' || $power < -50 && $idleType == 'discharge') && new \DateTime($config['timestamp']['date']) < new \DateTime('- 5 minutes')) || new \DateTime($config['timestamp']['date']) < new \DateTime('- 60 minutes'))) {
+                if ($msg === null && ((($power > 50 && $config['idleType'] == 'charge' || $power < -50 && $config['idleType'] == 'discharge' || $config['idleType'] == null) && new \DateTime($config['timestamp']['date']) < new \DateTime('- 5 minutes')) || new \DateTime($config['timestamp']['date']) < new \DateTime('- 60 minutes'))) {
                     $value = ['total_act_power' => $power];
                     $config['powerLimitFactor'] = 0;
                     $config['idleType'] = null;
