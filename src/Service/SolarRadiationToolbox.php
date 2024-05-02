f081f37573f5c9ed1175afa5def4988322c6a298
Markus Schafroth <markus.schafroth@3084.ch>
2024-05-02 16:00:25 +0200

    [Solar] bugfix

--- src/Service/SolarRadiationToolbox.php
+++ src/Service/SolarRadiationToolbox.php
@@ -119,53 +119,56 @@ class SolarRadiationToolbox
 
     private function calculateSolarPotentials(\DateTime $from = new \DateTime('-15 minutes'), \DateTime $until = new \DateTime('+ 2 days'))
     {
-        $weather = $this->em->getRepository(OpenWeatherMapDataLatest::class)->getLatest('onecallapi30')->getData();
         $this->solarPotentials = [];
-        $entries = array_merge([$weather['current']], $weather['hourly']);
-        $ePotTotCumulated = 0;
-        $prevTimestamp = null;
-        $prevPower = 0;
-        foreach ($entries as $entry) {
-            $timestamp = new \DateTime();
-            $timestamp->setTimestamp($entry['dt']);
-            if ($timestamp < $prevTimestamp || $timestamp <= $from || $timestamp >= $until) {
-                continue;
-            }
-            $sunPosition = $this->getSunPosition($timestamp, $entry['temp']+273.15, $entry['pressure']);
-            if ($sunPosition[0] < 10) {
-                $sunPosition[0] = 0;
-            }
-            $sunClimate = [
-                'datetime' => $timestamp,
-                'sunPosition' => $sunPosition,
-                'cloudiness' => $entry['clouds'],
-                'temperature' => $entry['temp']-273.15,
-                'humidity' => $entry['humidity'],
-            ];
-            $potentials = [];
-            $pPotTot = 0;
-            foreach ($this->connectors['smartfox']['pv']['panels'] as $pv) {
-                $pPot = $pv['pmax'] * sin(deg2rad($sunPosition[0])) * (2+abs(sin(deg2rad($sunPosition[0] - $pv['angle'])) * cos(deg2rad($sunPosition[1] - $pv['orientation']))))/3 * (110-$sunClimate['cloudiness'])/110;
-                if ($sunClimate['temperature'] > 25) {
-                    $pPot = $pPot - 1/3*($sunClimate['temperature'] - 25) * $pPot/100; // decrease by 1% per 3° above 25°C
+        $weatherEntity = $this->em->getRepository(OpenWeatherMapDataLatest::class)->getLatest('onecallapi30');
+        if ($weatherEntity) {
+            $weather = $weatherEntity->getData();
+            $entries = array_merge([$weather['current']], $weather['hourly']);
+            $ePotTotCumulated = 0;
+            $prevTimestamp = null;
+            $prevPower = 0;
+            foreach ($entries as $entry) {
+                $timestamp = new \DateTime();
+                $timestamp->setTimestamp($entry['dt']);
+                if ($timestamp < $prevTimestamp || $timestamp <= $from || $timestamp >= $until) {
+                    continue;
                 }
-                $potentials[] = [
-                    'panel' => $pv,
-                    'pPot' => $pPot,
+                $sunPosition = $this->getSunPosition($timestamp, $entry['temp']+273.15, $entry['pressure']);
+                if ($sunPosition[0] < 10) {
+                    $sunPosition[0] = 0;
+                }
+                $sunClimate = [
+                    'datetime' => $timestamp,
+                    'sunPosition' => $sunPosition,
+                    'cloudiness' => $entry['clouds'],
+                    'temperature' => $entry['temp']-273.15,
+                    'humidity' => $entry['humidity'],
                 ];
-                $pPotTot = $pPotTot + $pPot;
+                $potentials = [];
+                $pPotTot = 0;
+                foreach ($this->connectors['smartfox']['pv']['panels'] as $pv) {
+                    $pPot = $pv['pmax'] * sin(deg2rad($sunPosition[0])) * (2+abs(sin(deg2rad($sunPosition[0] - $pv['angle'])) * cos(deg2rad($sunPosition[1] - $pv['orientation']))))/3 * (110-$sunClimate['cloudiness'])/110;
+                    if ($sunClimate['temperature'] > 25) {
+                        $pPot = $pPot - 1/3*($sunClimate['temperature'] - 25) * $pPot/100; // decrease by 1% per 3° above 25°C
+                    }
+                    $potentials[] = [
+                        'panel' => $pv,
+                        'pPot' => $pPot,
+                    ];
+                    $pPotTot = $pPotTot + $pPot;
+                }
+                $pPotTot = min($pPotTot, $this->connectors['smartfox']['pv']['inverter']['pmax']);
+                if ($prevTimestamp !== null) {
+                    $timeInterval = ($timestamp->getTimestamp() - $prevTimestamp->getTimestamp())/3600; // interval in hours
+                } else {
+                    $timeInterval = 0;
+                }
+                $ePotTot = ($prevPower + $pPotTot) / 2 * $timeInterval;
+                $ePotTotCumulated = $ePotTotCumulated + $ePotTot;
+                $prevTimestamp = $timestamp;
+                $prevPower = $pPotTot;
+                $this->solarPotentials[$entry['dt']] = array_merge($sunClimate, ['pvPotentials' => $potentials], ['pPotTot' => $pPotTot], ['ePotTot' => $ePotTot], ['ePotTotCumulated' => $ePotTotCumulated]);
             }
-            $pPotTot = min($pPotTot, $this->connectors['smartfox']['pv']['inverter']['pmax']);
-            if ($prevTimestamp !== null) {
-                $timeInterval = ($timestamp->getTimestamp() - $prevTimestamp->getTimestamp())/3600; // interval in hours
-            } else {
-                $timeInterval = 0;
-            }
-            $ePotTot = ($prevPower + $pPotTot) / 2 * $timeInterval;
-            $ePotTotCumulated = $ePotTotCumulated + $ePotTot;
-            $prevTimestamp = $timestamp;
-            $prevPower = $pPotTot;
-            $this->solarPotentials[$entry['dt']] = array_merge($sunClimate, ['pvPotentials' => $potentials], ['pPotTot' => $pPotTot], ['ePotTot' => $ePotTot], ['ePotTotCumulated' => $ePotTotCumulated]);
         }
 
         return $this->solarPotentials;
