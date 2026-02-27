473cb6de2fe0d14461332049d1fa5d29b3799aad
Mara Schafroth <mara.schafroth@sbb.ch>
2026-02-27 14:41:51 +0100

    [SolRad] optimized training data retrieval

--- www/src/Repository/SmartFoxDataStoreRepository.php
+++ www/src/Repository/SmartFoxDataStoreRepository.php
@@ -301,7 +301,7 @@ class SmartFoxDataStoreRepository extends DataStoreBaseRepository
 
         $em = $this->getEntityManager();
         $connection = $em->getConnection();
-        $sql = "SELECT `json_value` FROM `data_store` WHERE `timestamp` >= '" . $date->format("Y-m-d") . "' AND `discr_type` LIKE 'smartfoxdatastore' AND MINUTE(timestamp) % 5 = 0";
+        $sql = "SELECT `json_value` FROM `data_store` WHERE `timestamp` >= '" . $date->format("Y-m-d") . "' AND `discr_type` LIKE 'smartfoxdatastore' AND MINUTE(timestamp) % 30 = 0 AND HOUR(timestamp) > 5 AND HOUR(timestamp) < 21";
         $stmt = $connection->prepare($sql);
         $resultSet = $stmt->executeQuery();
         $entities = $resultSet->fetchAll();
